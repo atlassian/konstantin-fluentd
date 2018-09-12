76dc453d21331c358bf0ca64d5a680804d5a3901
Denis Sorokin <dsorokin@atlassian.com>
2018-09-12 15:12:55 -0700

    Moved init file to install.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,14 +7,4 @@ define fluentd::config($config) {
     require => Class['Fluentd::Install'],
     notify  => Class['Fluentd::Service'],
   }
-
-  if $fluentd::init_manage {
-    file { '/etc/init.d/td-agent':
-      ensure  => file,
-      content => template('fluentd/td-agent.erb'),
-    }
-      ~> exec { '/usr/bin/systemctl daemon-reload':
-      refreshonly => true,
-    }
-  }
 }
--- manifests/install.pp
+++ manifests/install.pp
@@ -7,6 +7,16 @@ class fluentd::install inherits fluentd {
     ensure => $fluentd::package_ensure,
   }
 
+  -> if $fluentd::init_manage {
+    file { '/etc/init.d/td-agent':
+      ensure  => file,
+      content => template('fluentd/td-agent.erb'),
+    }
+      ~> exec { '/usr/bin/systemctl daemon-reload':
+      refreshonly => true,
+    }
+  }
+
   -> file { $fluentd::config_path:
     ensure  => directory,
     owner   => $fluentd::config_owner,
